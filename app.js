231fe06bd8b1c4ca9500766d3c42fad8e2bb067f
James Luong <jamesluong@hotmail.co.uk>
2022-08-16 14:59:15 +0100

    Switched to Ejs templating with partials

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 // uses express
-const express = require('express')
+const express = require('express');
+const morgan = require('morgan');
 const app = express()
+app.set('view engine', 'ejs');
 const port = 3000
 
+app.use(morgan('tiny'));
+
 app.get('/', (req, res) => {
-  res.sendFile('./views/index.html', {root: __dirname});
+  res.render('index');
 })
 
-app.get('/contact', (req, res) => {
-    res.sendFile('./views/contact.html', {root: __dirname});
+app.get('/new', (req, res) => {
+  res.render('new');
 })
-
-// redirects
-app.get('/contact-us', (req, res) => {
-    res.redirect('/contact');
-});
 
 // 404 page
 app.use('', (req, res) => {
-    res.status(404).sendFile('./views/404.html', {root: __dirname});
+    res.status(404).render('404');
 });
 
 app.listen(port, () => {
