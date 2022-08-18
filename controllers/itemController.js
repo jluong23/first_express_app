ea297f9467b55b9ee86be71864dedbac7242cfd1
James Luong <jamesluong@hotmail.co.uk>
2022-08-18 11:50:52 +0100

    Moved logic into controller

---
+++ controllers/itemController.js
@@ -0,0 +1,61 @@
+let content = [
+    {
+      id: 1,
+      title: "Item 1",
+      description: "Test 1"
+    },
+    {
+      id: 2,
+      title: "Item 2",
+      description: "Test 2"
+    }
+];
+
+const getAllItems = () => {
+    return content;
+}
+
+const item_index = (req, res) => {
+
+    res.render('index', {items: getAllItems()});
+
+}
+
+const item_details = (req,res) => {
+    const id = req.params.id;
+    let item = getAllItems().find((item) => item.id == id);
+    res.render('details', {item});
+}
+
+const item_create_form = (req, res) => {
+    res.render('create');
+
+}
+
+const item_create_post = (req, res) => { 
+    let items = getAllItems();
+    if(req.body){
+        // add id
+        req.body["id"] = items.length + 1;
+        // save
+        items.push(req.body);
+        // redirect to index page
+        res.redirect('/');
+    }
+}
+
+const item_delete = (req,res) => {
+    const id = req.params.id;
+    // update content to exclude the deleted item
+    content = content.filter((item) => item.id != id);
+    res.json(
+        {
+        redirect: '/', 
+        }
+    );
+}
+
+module.exports = {
+    item_index, item_details, item_create_post, item_create_form, item_delete
+
+}
--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -1,56 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const itemController = require("../controllers/itemController");
 
+router.get('/create', itemController.item_create_form);
 
-let content = [
-    {
-      id: 1,
-      title: "Item 1",
-      description: "Test 1"
-    },
-    {
-      id: 2,
-      title: "Item 2",
-      description: "Test 2"
-    }
-];
-
-router.get('/create', (req, res) => {
-    res.render('create');
-})
-
-router.get('/', (req,res) => {
-    res.render('index', {content: content});
-  })
+router.get('/', itemController.item_index);
   
+router.get('/:id', itemController.item_details);
 
-router.get('/:id', (req,res) => {
-    const id = req.params.id;
-    let item = content.find((item) => item.id == id);
-    res.render('details', {item});
-})
+router.delete('/:id', itemController.item_delete)
 
-router.delete('/:id', (req,res) => {
-    const id = req.params.id;
-    // update content to exclude the deleted item
-    content = content.filter((item) => item.id != id);
-    res.json(
-        {
-        redirect: '/', 
-        }
-    );
-})
-
-
-router.post('/create', (req, res) => { 
-    if(req.body){
-        // add id
-        req.body["id"] = content.length + 1;
-        // save content
-        content.push(req.body);
-        // redirect to index page
-        res.redirect('/');
-    }
-})
+router.post('/create', itemController.item_create_post);
 
 module.exports = router;
